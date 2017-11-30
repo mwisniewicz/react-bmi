586099162561b52b3d65f29fcadc4d7c5db1679b
Mateusz Wisniewicz <m.wisniewicz@gmail.com>
2017-11-30 23:24:59 +0100

    Adde inputs, main app class and css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,41 +1,187 @@
-"use strict";
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 console.log("Application starts!");
 
+var MainApplication = function (_React$Component) {
+    _inherits(MainApplication, _React$Component);
+
+    function MainApplication(props) {
+        _classCallCheck(this, MainApplication);
+
+        var _this = _possibleConstructorReturn(this, (MainApplication.__proto__ || Object.getPrototypeOf(MainApplication)).call(this, props));
+
+        _this.handleParams = _this.handleParams.bind(_this);
+        return _this;
+    }
+
+    _createClass(MainApplication, [{
+        key: 'handleParams',
+        value: function handleParams(mass, height) {
+            if (!mass) {
+                return 'Enter valid value';
+            } else {
+                var BMI = mass / (height * height);
+                return 'BMI: ' + BMI;
+            }
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+
+            var title = 'BMI';
+            var title_thin = 'CALCULATOR';
+            var subtitle = 'powered by React';
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(Header, { title: title, title_thin: title_thin, subtitle: subtitle }),
+                React.createElement(CalculateBMI, {
+                    handleParams: this.handleParams
+                })
+            );
+        }
+    }]);
+
+    return MainApplication;
+}(React.Component);
+
 var Header = function Header(props) {
     return React.createElement(
-        "header",
-        null,
+        'header',
+        { className: 'header-main' },
         React.createElement(
-            "h1",
-            null,
-            "BMI CALCULATOR"
-        ),
-        React.createElement(
-            "h3",
-            null,
-            "powered by React"
+            'div',
+            { className: 'container' },
+            React.createElement(
+                'h1',
+                null,
+                props.title,
+                React.createElement(
+                    'span',
+                    null,
+                    props.title_thin
+                )
+            ),
+            React.createElement(
+                'div',
+                { className: 'header-main-subtitle' },
+                React.createElement('span', { className: 'devicon-react-original' }),
+                React.createElement(
+                    'h3',
+                    null,
+                    props.subtitle
+                )
+            )
         )
     );
+};
+
+Header.defaultProps = {
+    title: 'BMI',
+    title_thin: 'CALCULATOR',
+    subtitle: 'powered by React'
 };
 
 var Footer = function Footer(props) {
     return React.createElement(
-        "footer",
+        'footer',
         null,
         React.createElement(
-            "h1",
+            'h1',
             null,
-            "BMI CALCULATOR"
+            'BMI CALCULATOR'
         ),
         React.createElement(
-            "h3",
+            'h3',
             null,
-            "powered by React"
+            'powered by React'
         )
     );
 };
 
+var CalculateBMI = function (_React$Component2) {
+    _inherits(CalculateBMI, _React$Component2);
+
+    function CalculateBMI(props) {
+        _classCallCheck(this, CalculateBMI);
+
+        var _this2 = _possibleConstructorReturn(this, (CalculateBMI.__proto__ || Object.getPrototypeOf(CalculateBMI)).call(this, props));
+
+        _this2.handleParams = _this2.handleParams.bind(_this2);
+        _this2.state = {
+            error: undefined
+        };
+        return _this2;
+    }
+
+    _createClass(CalculateBMI, [{
+        key: 'handleParams',
+        value: function handleParams(e) {
+            e.preventDefault();
+            var mass = e.target.elements.mass.value.trim();
+            var height = e.target.elements.height.value.trim();
+            var error = this.props.handleParams(mass, height);
+
+            this.setState(function () {
+                return { error: error };
+            });
+            console.log("work");
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { className: 'container' },
+                React.createElement(
+                    'form',
+                    { onSubmit: this.handleParams, className: 'bmi-form' },
+                    React.createElement(
+                        'div',
+                        { className: 'inputs' },
+                        React.createElement(
+                            'label',
+                            null,
+                            'Masa'
+                        ),
+                        React.createElement('input', { type: 'text', name: 'mass' })
+                    ),
+                    React.createElement(
+                        'div',
+                        { className: 'inputs' },
+                        React.createElement(
+                            'label',
+                            null,
+                            'Wzrost'
+                        ),
+                        React.createElement('input', { type: 'text', name: 'height' })
+                    ),
+                    React.createElement(
+                        'button',
+                        { className: 'button' },
+                        'Calculate'
+                    )
+                ),
+                this.state.error && React.createElement(
+                    'p',
+                    null,
+                    this.state.error
+                )
+            );
+        }
+    }]);
+
+    return CalculateBMI;
+}(React.Component);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(React.createElement(Header, null), appRoot);
+ReactDOM.render(React.createElement(MainApplication, null), appRoot);
--- src/app.js
+++ src/app.js
@@ -1,12 +1,55 @@
 console.log("Application starts!");
 
+class MainApplication extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleParams = this.handleParams.bind(this);
+    }
+
+    handleParams(mass,height) {
+        if (!mass) {
+            return 'Enter valid value';
+        } else {
+            const BMI = mass/(height*height);
+            return 'BMI: '+BMI;
+        }
+
+    }
+
+    render() {
+
+        const title = 'BMI';
+        const title_thin = 'CALCULATOR';
+        const subtitle = 'powered by React';
+        return (
+            <div>
+                <Header title={title} title_thin={title_thin} subtitle={subtitle} />
+                <CalculateBMI 
+                    handleParams={this.handleParams}
+                />
+            </div>
+        );
+    }
+}
+
 const Header = (props) => {
     return (
-        <header>
-            <h1>BMI CALCULATOR</h1>
-            <h3>powered by React</h3>
+        <header className="header-main">
+            <div className="container">
+            <h1>{props.title}<span>{props.title_thin}</span></h1>
+            <div className="header-main-subtitle">
+                <span className="devicon-react-original"></span>
+                <h3>{props.subtitle}</h3>
+            </div>
+            </div>
         </header>
     );
+};
+
+Header.defaultProps = {
+    title: 'BMI',
+    title_thin: 'CALCULATOR', 
+    subtitle: 'powered by React'
 };
 
 const Footer = (props) => {
@@ -18,6 +61,45 @@ const Footer = (props) => {
     );
 };
 
+class CalculateBMI extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleParams = this.handleParams.bind(this);
+        this.state = {
+            error: undefined
+        };
+    }
+
+    handleParams(e) {
+        e.preventDefault();
+        const mass = e.target.elements.mass.value.trim();
+        const height = e.target.elements.height.value.trim();
+        const error = this.props.handleParams(mass,height);
+
+        this.setState(() => ({error}));
+        console.log("work");
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <form onSubmit={this.handleParams} className="bmi-form">
+                    <div className="inputs">
+                        <label>Masa</label><input type="text" name="mass" />
+                    </div>
+                    <div className="inputs">
+                    <label>Wzrost</label><input type="text" name="height" />
+                    </div>
+                    <button className="button">Calculate</button>
+                </form>
+                {this.state.error && <p>{this.state.error}</p>}
+            </div>
+        );
+    }
+
+
+}
+
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(<Header />, appRoot);
+ReactDOM.render(<MainApplication />, appRoot);
